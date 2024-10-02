82fd3786fab47a710591ffdf94ca12f3ef463a4f
AtharvaJoshi16 <aj962788@gmail.com>
2024-10-02 18:53:40 +0530

    added google analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,6 @@
+import GoogleAnalytics from "@/customs/google-analytics";
 import type { Metadata } from "next";
+import { Suspense } from "react";
 import { ClientLayout } from "./clientLayout";
 import "./globals.css";
 
@@ -14,6 +16,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Suspense fallback={null}>
+        <GoogleAnalytics GA_MEASUREMENT_ID={process.env.GA_MEASUREMENT_ID!} />
+      </Suspense>
       <body
         className={`no-scrollbar antialiased bg-slate-800 m-2 sm:m-4 md:m-6 lg:m-8 xl:m-10`}
       >
